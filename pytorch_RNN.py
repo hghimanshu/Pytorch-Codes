957ad6f06e7cc7f47a2666deb38abc9c0ec1bbe0
Himanshu Garg <30381781+hghimanshu@users.noreply.github.com>
2019-02-18 11:03:52 +0530

    Update pytorch_RNN.py

--- pytorch_RNN.py
+++ pytorch_RNN.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb  6 17:03:58 2019
-
-@author: techject
-"""
-
 from matplotlib import pyplot as plt
 import numpy as np
 import torch
